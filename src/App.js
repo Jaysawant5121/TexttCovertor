98fa76315fba1120dab67ddedc76e8265edee0ef
jaysawaant5121 <jay.sawant@staragri.com>
2023-12-25 00:54:58 +0530

    React 1st project,so basiclally it is web app which converts the lower case to upper case vice versa, dark and light theme mode added to project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+import Alert from './components/Alert';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.background = 'grey';
+      showAlert('Dark mode enabled', 'success');
+    } else {
+      setMode('light');
+      document.body.style.background = 'white';
+      showAlert('Light mode enabled', 'success');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <BrowserRouter> 
+        <Navbar title="Main page" mode={mode} toggleMode={toggleMode} />
+        <Alert popalert={alert} />
+        <Routes> 
+        <Route path="/TextForm" element={<TextForm showAlert={showAlert} heading="Enter the text here " mode={mode} />}/>
+        <Route path="/about" element={<About about="It is TextUtils coverts the text" />}/>
+
+        </Routes>
+        </BrowserRouter>
+       
+      </>
   );
 }
 
